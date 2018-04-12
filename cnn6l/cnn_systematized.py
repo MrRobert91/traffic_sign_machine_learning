05b8e2185e3fc5da09dcf7a5a8eade5a16b887d7
MrRobert91 <davidrobertnunez@gmail.com>
2018-04-12 16:55:25 +0200

    cnn classifier

--- cnn6l/cnn_systematized.py
+++ cnn6l/cnn_systematized.py
@@ -77,19 +77,19 @@ def cnn_model():
                      activation='relu'))
     model.add(Conv2D(32, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
+    model.add(Dropout(0.5))#antes 0.2
 
     model.add(Conv2D(64, (3, 3), padding='same',
                      activation='relu'))
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
+    model.add(Dropout(0.5))#antes 0.2
 
     model.add(Conv2D(128, (3, 3), padding='same',
                      activation='relu'))
     model.add(Conv2D(128, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
+    model.add(Dropout(0.5))#antes 0.2
 
     model.add(Flatten())
     model.add(Dense(512, activation='relu'))
