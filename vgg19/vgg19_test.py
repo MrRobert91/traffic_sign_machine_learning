0ffbfc91e3082904b09ade0f184d205d779cb08b
MrRobert91 <davidrobertnunez@gmail.com>
2018-05-03 06:35:57 +0200

    cnn classifier

--- vgg19/vgg19_test.py
+++ vgg19/vgg19_test.py
@@ -62,8 +62,8 @@ logging.basicConfig(level=logging.DEBUG,
                     filename = fichero_log,
                     filemode = 'a',)
 # start time
-print ("[STATUS] xception test - start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
-logging.info(" xception test - start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
+print ("[STATUS] vgg19 test - start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
+logging.info(" vgg19 test - start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
 start = time.time()
 
 # load the trained logistic regression classifier
