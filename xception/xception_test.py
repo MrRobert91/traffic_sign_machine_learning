984762f0c9c8e97a72226d531e287c20c26e09d3
MrRobert91 <davidrobertnunez@gmail.com>
2018-05-03 08:04:25 +0200

    cnn classifier

--- xception/xception_test.py
+++ xception/xception_test.py
@@ -160,7 +160,7 @@ logging.info("[INFO] test labels : {}".format(y_test.shape))
 result = classifier.score(X_test, y_test)
 print("result: "+ str(result))
 
-score, test_accuracy = classifier.evaluate(X_test, y_test, verbose=1)
+test_accuracy = classifier.accuracy_score(X_test, y_test)
 print("test_accuracy final del modelo en test: %.2f%% " % (test_accuracy))
 logging.info("test_accuracy final del modelo en test: %.2f%% " % (test_accuracy))
 
