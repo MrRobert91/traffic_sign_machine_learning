931e3fdf16bee0ecacdeca256090960f18e96e6b
MrRobert91 <davidrobertnunez@gmail.com>
2018-05-03 17:37:32 +0200

    cnn classifier

--- vgg19/vgg19_from_scratch.py
+++ vgg19/vgg19_from_scratch.py
@@ -75,7 +75,7 @@ logging.basicConfig(level=logging.DEBUG,
                     filename = fichero_log,
                     filemode = 'a',)
 # start time
-print ("[STATUS] vgg19 from scratch - start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
+print ("[STATUS] -------------vgg19 from scratch - start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
 logging.info(" start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
 start = time.time()
 
