8de81c6be9714d9a0cb180cffe0582f3e4db60b7
MrRobert91 <davidrobertnunez@gmail.com>
2018-05-03 17:38:25 +0200

    cnn classifier

--- xception/xception_test.py
+++ xception/xception_test.py
@@ -62,7 +62,7 @@ logging.basicConfig(level=logging.DEBUG,
                     filename = fichero_log,
                     filemode = 'a',)
 # start time
-print ("[STATUS] xception test - start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
+print ("[STATUS] ------------ xception test - start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
 logging.info(" xception test - start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
 start = time.time()
 
