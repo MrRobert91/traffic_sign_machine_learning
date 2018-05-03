9808060300fa99201d716080276baadabfbcbd24
MrRobert91 <davidrobertnunez@gmail.com>
2018-05-03 17:37:15 +0200

    cnn classifier

--- vgg19/vgg19_systematized.py
+++ vgg19/vgg19_systematized.py
@@ -61,7 +61,7 @@ logging.basicConfig(level=logging.DEBUG,
                     filename = fichero_log,
                     filemode = 'a',)
 # start time
-print ("[STATUS] start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
+print ("[STATUS]-------vgg19 systematized - start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
 logging.info(" start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
 start = time.time()
 
