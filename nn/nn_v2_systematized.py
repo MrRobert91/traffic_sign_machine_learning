2deab2ebb77a434eeee73b7300c93c8802de2631
MrRobert91 <davidrobertnunez@gmail.com>
2018-05-19 18:44:55 +0200

    cnn classifier

--- nn/nn_v2_systematized.py
+++ nn/nn_v2_systematized.py
@@ -203,7 +203,7 @@ for train_index, test_index in skf.split(X, Y):
 
 
 
-    nn_classifier = baseline_nn_model()
+    nn_classifier = nn_model()
 
     # vamos a entrenar nuestro modelo con SGD + momentum
     sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
