345b6961187bb14f0efa23966086f7498720a201
MrRobert91 <davidrobertnunez@gmail.com>
2018-05-19 15:35:38 +0200

    cnn classifier

--- rf/random_forest_systematized.py
+++ rf/random_forest_systematized.py
@@ -35,12 +35,12 @@ import logging
 
 
 #local
-#code_path= "/home/david/PycharmProjects/traffic_sign_machine_learning/rf/"
-#dataset_path="/home/david/Escritorio/TFG/Pruebas"
+code_path= "/home/david/PycharmProjects/traffic_sign_machine_learning/rf/"
+dataset_path="/home/david/Escritorio/TFG/Pruebas"
 
 #Corleone
-code_path="/home/drobert/tfg/traffic_sign_machine_learning/rf/"
-dataset_path='/home/drobert/tfg/'
+#code_path="/home/drobert/tfg/traffic_sign_machine_learning/rf/"
+#dataset_path='/home/drobert/tfg/'
 
 # Fichero de log
 fichero_log = (code_path +'rf.log')
@@ -143,7 +143,7 @@ clf_list = []
 filename_clf_list = []
 
 n_trees = 300
-depth = 13
+depth = 12
 fold = 1
 splits = 3
 
