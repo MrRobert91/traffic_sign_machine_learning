3a7f0fe8b43ac84a43d9eeff887b830e1d0b3704
MrRobert91 <davidrobertnunez@gmail.com>
2018-05-25 01:59:31 +0200

    cnn classifier

--- cnn6l/cnn_multi_scale_res.py
+++ cnn6l/cnn_multi_scale_res.py
@@ -80,22 +80,22 @@ def res_multi_stage2_GAPool():
     input_tensor = Input(shape=(IMG_SIZE, IMG_SIZE, 3), name='4d_input')
 
     #1ª Etapa: la salida de esta etapa va
-    x = layers.Conv2D(32, (3, 3), padding='same', activation='relu') (input_tensor)
+    x = layers.SeparableConv2D(32, (3, 3), padding='same', activation='relu') (input_tensor)
     x = layers.MaxPooling2D(pool_size=(2, 2))(x)
     x = layers.Dropout(0.3)(x)
 
 
     #2ª Etapa
-    x_principal = layers.Conv2D(64, (3, 3), padding='same', activation='relu')(x)
+    x_principal = layers.SeparableConv2D(64, (3, 3), padding='same', activation='relu')(x)
     x_principal = layers.MaxPooling2D(pool_size=(2, 2))(x_principal)
     x_principal = layers.Dropout(0.4)(x_principal)
-    x_flatten_2 = layers.GlobalAveragePooling2D()(x_principal)
+    x_flatten_2 = layers.Flatten()(x_principal)
 
     #3ª Etapa
-    x_principal = layers.Conv2D(128, (3, 3), padding='same', activation='relu')(x_principal)
+    x_principal = layers.SeparableConv2D(128, (3, 3), padding='same', activation='relu')(x_principal)
     x_principal = layers.MaxPooling2D(pool_size=(2, 2))(x_principal)
     x_principal = layers.Dropout(0.4)(x_principal)
-    x_flatten_3 = layers.GlobalAveragePooling2D()(x_principal)
+    x_flatten_3 = layers.Flatten()(x_principal)
 
 
     # Etapa de concatenacion
