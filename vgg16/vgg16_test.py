0f387a72758896cf51be87fdc3cc222e91fe2c39
MrRobert91 <davidrobertnunez@gmail.com>
2018-06-05 02:08:36 +0200

    cnn classifier

--- vgg16/vgg16_test.py
+++ vgg16/vgg16_test.py
@@ -33,6 +33,8 @@ import time
 import logging
 import pandas as pd
 from skimage import io
+from keras.models import load_model
+from sklearn.model_selection import KFold
 
 # load the user configs
 with open('conf.json') as f:
@@ -75,6 +77,8 @@ base_model = VGG16(weights=weights)
 model = Model(input=base_model.input, output=base_model.get_layer('fc1').output)
 image_size = (224, 224)
 
+modelo="vgg16_fe"
+NUM_CLASSES = 43
 
 # get all the train labels
 #train_labels = os.listdir(train_path)
@@ -132,6 +136,55 @@ logging.info("[INFO] test data   : {}".format(X_test.shape))
 logging.info("[INFO] test labels : {}".format(y_test.shape))
 #print("Tiene que ser algo como: (31367, 2048) y las labels : (7842,) ")
 
+
+#Vamos a dividir el conjunto de test en 5
+
+kf = KFold(n_splits=5,random_state=13) # Define the split - into 2 folds
+kf.get_n_splits(X_test) # returns the number of splitting iterations in the cross-validator
+
+print(kf)
+
+fold =1
+accuracy_list = []
+
+for train_index, test_index in kf.split(X_test):
+    _, X_test_fold = X_test[train_index], X_test[test_index]
+    _, y_test_fold = y_test[train_index], y_test[test_index]
+
+
+    # --Para Random Forest-- Cambiamos los formatos de entrada de las imagenes para que sea una matriz bidimensional
+    #X_test = X_test.reshape((-1, 48 * 48 * 3)).astype(np.float32)
+
+    # --Para resto de modelos-- Los targets tienen que estar en formato one target
+    y_test_fold_one_target = np.eye(NUM_CLASSES, dtype='uint8')[y_test_fold]
+
+    # Para RF
+    # test_accuracy = loaded_model.score(X_test, y_test)
+
+    # Para resto de modelos
+    #test_accuracy = loaded_model.evaluate(X_test_fold, y_test_fold_one_target, verbose=1)
+
+    #Para Feature extraction
+    test_accuracy = classifier.score(X_test, y_test)
+    accuracy_list.append(test_accuracy[1] * 100)
+
+    print(str(fold)+" Resultado final del modelo en test: %.2f%% " % (test_accuracy[1] * 100))
+    logging.info(str(fold)+" Resultado final del modelo en test: %.2f%% " % (test_accuracy[1] * 100))
+
+    fold += 1
+
+
+
+precision_media = (np.mean(accuracy_list))
+desviacion_standar = (np.std(accuracy_list))
+
+print("mean_accuarcy de" +modelo+": %.2f%% (+/- %.2f%%)" % (np.mean(accuracy_list), np.std(accuracy_list)))
+logging.info("mean_accuarcy de " +modelo+": %.2f%% (+/- %.2f%%)" % (np.mean(accuracy_list), np.std(accuracy_list)))
+
+
+
+'''
+
 #result on the logistic regression classifier
 
 result = classifier.score(X_test, y_test)
@@ -139,6 +192,28 @@ result = classifier.score(X_test, y_test)
 print("test_accuracy final del modelo en test: %.2f%% " % (result*100))
 logging.info("test_accuracy final del modelo en test: %.2f%% " % (result *100))
 
+
+today_date = datetime.date.today().strftime("%d-%m-%Y")
+best_model_filename= (model+"_test_acc_%.2f%%_%s.h5" % (result * 100, today_date))
+
+#pickle.dump(best_model, open((code_path + str(best_model_filename)), 'wb'))
+
+#guardar con h5 no funciona por tener un metodo custom de accuracy
+classifier.save(best_model_filename)
+
+print("Accuracy en test : %.2f%%" % (result * 100))
+
+logging.info("Accuracy en test : %.2f%%" % ( result * 100))
+
+
+#Comprobamos que el modelo cargado tiene la misma precision
+
+#loaded_model = pickle.load(open(best_model_filename, 'rb'))
+loaded_model = load_model(best_model_filename)# No funciona con custom metrics
+
+loaded_model_test_accuracy = loaded_model.score(X_test, y_test)
+print("Loaded_model accuracy en test : %s: %.2f%%" % (loaded_model.metrics_names[1], loaded_model_test_accuracy[1] * 100))
+'''
 
 end = time.time()
 print ("[STATUS] end time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
