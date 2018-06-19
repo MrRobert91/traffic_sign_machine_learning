db9f037876913479d1d2f5b31800d3a97481c119
MrRobert91 <davidrobertnunez@gmail.com>
2018-06-19 04:51:32 +0200

    cnn classifier

--- cnn6l/test_belgium_dataset.py
+++ cnn6l/test_belgium_dataset.py
@@ -42,7 +42,7 @@ logging.info(" ---------"+modelo+"- start time - {}".format(datetime.datetime.no
 
 
 NUM_CLASSES = 43
-IMG_SIZE = 48 #48
+IMG_SIZE = 32 #48
 
 
 # Funcion para preprocesar las imagenes
@@ -69,7 +69,7 @@ os.chdir(code_path)
 #cnn_v1_48_epochs20_test_acc_96.89%_28-05-2018.h5
 #cnn_multi_scale_epochs50_test_acc_95.39%_25-05-2018.h5
 
-modelname = "mini_vgg_48_epochs20_test_acc_97.37%_30-05-2018.h5"
+modelname = "cnn_multi_scale_epochs50_test_acc_95.39%_25-05-2018.h5"
 
 #Para random forest
 #loaded_model = pickle.load(open(modelname, 'rb'))
