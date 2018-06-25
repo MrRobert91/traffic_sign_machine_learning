72458932c012c3d66f21fbc1454dd9cfbf889dfd
MrRobert91 <davidrobertnunez@gmail.com>
2018-06-25 06:05:50 +0200

    cnn classifier

--- resnet50/resnet50_systematized.py
+++ resnet50/resnet50_systematized.py
@@ -61,8 +61,8 @@ logging.basicConfig(level=logging.DEBUG,
                     filename = fichero_log,
                     filemode = 'a',)
 # start time
-print ("[STATUS] start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
-logging.info(" start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
+print ("[STATUS] -------- resnet50 systematized - start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
+logging.info(" -------- resnet50 systematized start time - {}".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
 start = time.time()
 
 # create the pretrained models
