174e6862ff6acd3f09eb1245bc47776a2e91cf5e
MrRobert91 <davidrobertnunez@gmail.com>
2018-07-07 06:45:51 +0200

    cnn classifier

--- resnet50/resnet50_test.py
+++ resnet50/resnet50_test.py
@@ -131,7 +131,7 @@ print("[INFO] test data   : {}".format(X_test.shape))
 print("[INFO] test labels : {}".format(y_test.shape))
 logging.info("[INFO] test data   : {}".format(X_test.shape))
 logging.info("[INFO] test labels : {}".format(y_test.shape))
-#print("Tiene que ser algo como: (31367, 2048) y las labels : (7842,) ")
+
 
 #result on the logistic regression classifier
 
